e7828405f87b02c4c9650d08cefb442ce54790a3
sergio-slima <sergio_silvalima@hotmail.com>
2023-02-02 14:15:53 -0300

    config connection FB

--- DataModule/UDM.dfm
+++ DataModule/UDM.dfm
@@ -1,12 +1,24 @@
 object DM: TDM
   OldCreateOrder = False
+  OnCreate = DataModuleCreate
   Height = 266
   Width = 336
   object Conn: TFDConnection
+    Params.Strings = (
+      'Database=D:\Sergio\Projetos\bodyfit\servidor\DB\BANCO.FDB'
+      'User_Name=sysdba'
+      'Password=senhasys'
+      'Server=LocalHost'
+      'DriverID=FB')
     TxOptions.Isolation = xiDirtyRead
     ConnectedStoredUsage = []
     LoginPrompt = False
+    BeforeConnect = ConnBeforeConnect
     Left = 48
     Top = 40
+  end
+  object FDPhysFBDriverLink: TFDPhysFBDriverLink
+    Left = 48
+    Top = 112
   end
 end
--- DataModule/UDM.pas
+++ DataModule/UDM.pas
@@ -6,12 +6,17 @@ uses
   System.SysUtils, System.Classes, FireDAC.Stan.Intf, FireDAC.Stan.Option,
   FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
   FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.FMXUI.Wait,
-  Data.DB, FireDAC.Comp.Client;
+  Data.DB, FireDAC.Comp.Client, FireDAC.Phys.FB, FireDAC.Phys.FBDef,
+  FireDAC.Phys.IBBase, DataSet.Serialize.Config;
 
 type
   TDM = class(TDataModule)
     Conn: TFDConnection;
+    FDPhysFBDriverLink: TFDPhysFBDriverLink;
+    procedure DataModuleCreate(Sender: TObject);
+    procedure ConnBeforeConnect(Sender: TObject);
   private
+    procedure CarregarConfigDB(Connection: TFDConnection);
     { Private declarations }
   public
     { Public declarations }
@@ -25,5 +30,37 @@ implementation
 {%CLASSGROUP 'FMX.Controls.TControl'}
 
 {$R *.dfm}
+
+procedure TDM.CarregarConfigDB(Connection: TFDConnection);
+begin
+  Connection.DriverName := 'FB';
+
+  with Connection.Params do
+  begin
+    Clear;
+    Add('DriverID=FB');
+    Add('Database=D:\Sergio\Projetos\bodyfit\servidor\DB\BANCO.FDB');
+    Add('User_Name=SYSDBA');
+    Add('Password=senhasys');
+    Add('Port=3050');
+    Add('Server=localhost');
+    Add('Protocol=TCPIP');
+  end;
+
+  FDPhysFBDriverLink.VendorLib := 'C:\Program Files (x86)\Firebird\Firebird_3_0\fbclient.dll';
+end;
+
+procedure TDM.ConnBeforeConnect(Sender: TObject);
+begin
+  CarregarConfigDB(Conn);
+end;
+
+procedure TDM.DataModuleCreate(Sender: TObject);
+begin
+  TDataSetSerializeConfig.GetInstance.CaseNameDefinition := cndLower;
+  TDataSetSerializeConfig.GetInstance.Import.DecimalSeparator := '.';
+
+  Conn.Connected := True;
+end;
 
 end.
